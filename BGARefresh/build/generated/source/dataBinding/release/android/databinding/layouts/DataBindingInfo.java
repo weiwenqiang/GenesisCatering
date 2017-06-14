4441b623bf3d66e492d8232a65f7b250382b4c99
weiwenqiang <wwqweiwenqiang@qq.com>
2017-06-14 11:09:24 +0800

    分包，导库，导工具类

---
+++ BGARefresh/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="81f8f35a-b52f-416e-b12b-7d83eddcd404", modulePackage="cn.bingoogolapple.refreshlayout", sdkRoot="E:\\Android\\SDK",layoutInfoDir="F:\\zp\\studio\\Genesis\\GenesisCatering\\BGARefresh\\build\\intermediates\\data-binding-info\\release",exportClassListTo="F:\\zp\\studio\\Genesis\\GenesisCatering\\BGARefresh\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
